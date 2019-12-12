6849585044c561d86ad55088ef0f74af3963bffa
jetigenov <jetigenov@jetigenovs-MacBook-Pro.local>
2019-12-12 22:31:43 +0600

    order summary done

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from .views import (
     HomeView,
     add_to_cart,
     remove_from_cart,
-    OrderSummaryView
+    OrderSummaryView,
+    remove_single_item_from_cart
 
 )
 
@@ -19,5 +20,6 @@ urlpatterns = [
     path('products/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+         name='remove-single-item-from-cart'),
 ]
