6d15aae972c03539c9cb7a10fb4207781d843778
jetigenov <jetigenov@jetigenovs-MacBook-Pro.local>
2019-12-13 15:47:34 +0600

    form and checkbox

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from django_countries.fields import CountryField
 
+PAYMENT_CHOICES =(
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Bishkek street'
+    }))
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or suite'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(attr={
+        'class': 'custom-select d-block w-100'
+    })
     zip = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    same_billing_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
