26dce82f10b3342baba322498debc0eb51ef23f7
jetigenov <jetigenov@jetigenovs-MacBook-Pro.local>
2019-12-16 15:48:43 +0600

    image func

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,3 +9,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls', namespace='core'))
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
