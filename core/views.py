6c58d564562c811a06c1d506f06a258d2b465f08
jetigenov <jetigenov@jetigenovs-MacBook-Pro.local>
2019-12-16 15:20:24 +0600

    billing address with functionality

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from .forms import CheckoutForm
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
 
 
 def products(request):
@@ -30,10 +30,36 @@ class CheckoutView(View):
 
     def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST or None)
-        if form.is_valid():
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            if form.is_valid():
+                street_address = form.cleaned_data.get('street_address')
+                apartment_address = form.cleaned_data.get('apartment_address')
+                country = form.cleaned_data.get('country')
+                zip = form.cleaned_data.get('zip')
+                # Todo: add functionality for these fields
+                # same_shipping_address = form.cleaned_data.get('same_shipping_address')
+                # save_info = form.cleaned_data.get('save_info')
+                # payment_option = form.cleaned_data.get('payment_option')
+                billing_address = BillingAddress(
+                    user=self.request.user,
+                    street_address=street_address,
+                    apartment_address=apartment_address,
+                    country=country,
+                    zip=zip
+                )
+                billing_address.save()
+                order.billing_address = billing_address
+                order.save()
+                # Todo: add redirect to the selected payment option
+                return redirect('core:checkout')
+            messages.warning(self.request, "Failed checkout")
             return redirect('core:checkout')
-        messages.warning(self.request, "Failed checkout")
-        return redirect('core:checkout')
+
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You don not have an active order")
+            return redirect("core:order-summary")
+
 
 
 
@@ -54,7 +80,7 @@ class OrderSummaryView(LoginRequiredMixin, View):
             }
             return render(self.request, 'order_summary.html', context)
         except ObjectDoesNotExist:
-            messages.error(self.request, "You don not have active order")
+            messages.error(self.request, "You don not have an active order")
             return redirect("/")
 
 class ItemDetailView(DetailView):
